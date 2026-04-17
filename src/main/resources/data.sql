9537a566eebf2f00a21bf26870569de555123a4a
girishbendale2 <girishbendale2@gmail.com>
2026-04-17 20:26:57 +0530

    webapp | fixed correction as per comments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,19 @@
-INSERT INTO transaction_entity (customer_id, amount, transaction_date) VALUES
-('C001', 120.00, '2026-01-15'),
-('C001', 80.00, '2026-01-20'),
-('C001', 150.00, '2026-02-10'),
-('C001', 60.00, '2026-02-15'),
-('C001', 200.00, '2026-03-05'),
-('C002', 90.00, '2026-01-12'),
-('C002', 130.00, '2026-02-18'),
-('C002', 70.00, '2026-03-22');
+CREATE TABLE transaction (
+    customer_id INT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    transaction_date DATE NOT NULL
+);
+
+
+INSERT INTO transaction (customer_id, amount, transaction_date) VALUES
+('1', 120.00, '2026-01-15'),
+('2', 80.00, '2026-01-20'),
+('3', 150.00, '2026-02-10'),
+('4', 60.00, '2026-02-15'),
+('5', 200.00, '2026-03-05'),
+('6', 90.00, '2026-01-12'),
+('7', 130.00, '2026-02-18'),
+('8', 70.00, '2026-03-22');
+
+commit;
+
